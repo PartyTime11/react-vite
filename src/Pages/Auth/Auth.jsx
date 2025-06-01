d133ac8a4f3200bb4782f27bd49173afd2a35a53
PartyTime11 <e.barzdun@yandex.ru>
2025-06-01 18:18:45 +1000

    Add pages

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Auth =  () => {
+    return (
+        <div>
+            auth page
+            <Outlet /> 
+        </div>
+    )
+}
+
+export default Auth
